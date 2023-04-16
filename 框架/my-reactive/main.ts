a8a37204821dc3af1ed91e9a4d87588dcc24164d
kcp <2842961263@qq.com>
2023-04-16 11:46:36 +0800

    feat: clean up (分支切换问题)

--- 框架/my-reactive/main.ts
+++ 框架/my-reactive/main.ts
@@ -1,5 +1,11 @@
-import { createSignal } from "./reactive";
+import { createEffect, createSignal } from "./reactive";
 
 const [count, setCount] = createSignal<number>(0);
 
 console.log(count()); // 0
+
+createEffect(() => {
+  console.log(count());
+});
+
+setCount(1); // 1
--- 框架/my-reactive/reactive.ts
+++ 框架/my-reactive/reactive.ts
@@ -1,20 +1,36 @@
 type Observer = {
   execute: Function;
+  dependencies: Set<Set<Observer>>;
 };
 
 const context = new Array<Observer>(); // 用于存储当前正在执行的函数
 
+// 处理分支切换的问题
+function cleanup(observer: Observer) {
+  for (const subscriptions of observer.dependencies)
+    subscriptions.delete(observer);
+}
+
+function subscribe(observer: Observer, subscriptions: Set<Observer>) {
+  subscriptions.add(observer);
+  observer.dependencies.add(subscriptions);
+}
+
+// every signals is dependent on the context
 export function createSignal<T>(value: T): [() => T, (newValue: T) => void] {
-  const description = new Set<Observer>(); // 存储响应式变量会影响的函数
+  const subscriptions = new Set<Observer>(); // 存储响应式变量会影响的函数
 
   const read = () => {
     if (context.length) {
-      description.add(context[context.length - 1]); // 将当前正在执行的函数添加到响应式变量的依赖列表中
+      const observer = context[context.length - 1];
+      subscribe(observer, subscriptions); // 将当前正在执行的函数添加到响应式变量的依赖列表中
+      // subscriptions.add(context[context.length - 1]); // 将当前正在执行的函数添加到响应式变量的依赖列表中
     }
     return value;
   };
   const write = (newValue: T) => {
-    for (const observer of description) observer.execute(); // 通知所有依赖该响应式变量的函数重新执行
+    // 这里的 ... 通过创建一个新的数组,避免在循环中修改 subscriptions导致的无限循环
+    for (const observer of [...subscriptions]) observer.execute(); // 通知所有依赖该响应式变量的函数重新执行
     return (value = newValue);
   };
   return [read, write];
@@ -23,11 +39,12 @@ export function createSignal<T>(value: T): [() => T, (newValue: T) => void] {
 export function createEffect(fn: Function) {
   const effect = {
     execute() {
+      cleanup(effect); // 清除上一次执行的函数产生的副作用
       context.push(effect); // 将当前正在执行的函数添加到上下文中
       fn();
       context.pop(); // 执行完毕后从上下文中移除
     },
+    dependencies: new Set<Set<Observer>>(),
   };
-
   effect.execute();
 }
