f4429cd5ce8203f38fe30245f49b2a5d8a9a9648
kcp <2842961263@qq.com>
2023-05-04 10:43:36 +0800

    feat: 添加一些手写题

---
+++ js-ts/常见手写题/promise/all.js
@@ -0,0 +1,41 @@
+function all(promises){
+  if( !Array.isArray(promises) ){
+    throw new TypeError('promises must be an array');
+  }
+  return new Promise((resolve,reject)=>{
+    let promiseLength = promises.length;
+    let result = [];
+    let count = 0;
+    for(let i = 0; i < promiseLength; i ++){
+      Promise.resolve(promises[i]).then((res)=>{
+        count ++;
+        result.push(res);
+        if(count === promiseLength){
+          resolve(result);
+        }
+      }, (err) =>{
+        reject(err);
+      });
+    }
+  })
+}
+
+// test
+let p1 = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve(1);
+  },1000);
+})
+let p2 = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve(2);
+  },2000);
+})
+let p3 = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve(3);
+  },3000);
+})
+all([p1,p2,p3]).then((res)=>{
+  console.log(res);
+})
