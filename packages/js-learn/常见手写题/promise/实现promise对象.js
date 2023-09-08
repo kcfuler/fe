75d3aceb8e23bb4ef801bbfb0832fc4e6db1f74c
kcfuler <kcfuler@gmail.com>
2023-09-08 15:28:29 +0800

    feat: 通过process.nextTick()实现异步调用

--- packages/js-learn/常见手写题/promise/实现promise对象.js
+++ packages/js-learn/常见手写题/promise/实现promise对象.js
@@ -1,6 +1,6 @@
-const PENDING = 'pending'
-const FULFILLED = 'fulfilled'
-const REJECTED = 'rejected'
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
 
 class MyPromise {
   constructor(executor) {
@@ -11,20 +11,24 @@ class MyPromise {
     this.onRejectedCallbacks = [];
 
     const resolve = (value) => {
-      if (this.state === PENDING) {
-        this.state = FULFILLED;
-        this.value = value;
-        this.onFulfilledCallbacks.forEach(fn => fn());
-      }
-    }
+      process.nextTick(() => {
+        if (this.state === PENDING) {
+          this.state = FULFILLED;
+          this.value = value;
+          this.onFulfilledCallbacks.forEach((fn) => fn());
+        }
+      });
+    };
 
     const reject = (reason) => {
-      if (this.state === PENDING) {
-        this.state = REJECTED;
-        this.reason = reason;
-        this.onRejectedCallbacks.forEach(fn => fn());
-      }
-    }
+      process.nextTick(() => {
+        if (this.state === PENDING) {
+          this.state = REJECTED;
+          this.reason = reason;
+          this.onRejectedCallbacks.forEach((fn) => fn());
+        }
+      });
+    };
 
     try {
       executor(resolve, reject);
@@ -33,50 +37,55 @@ class MyPromise {
     }
   }
 
-  
-
   then = (onFulfilled, onRejected) => {
     return new MyPromise((resolve, reject) => {
       if (this.state === FULFILLED) {
-        try {
-          const x = onFulfilled(this.value);
-          resolve(x);
-        } catch(err) {
-          reject(err);
-        }
+        process.nextTick(() => {
+          try {
+            const x = onFulfilled(this.value);
+            resolve(x);
+          } catch (err) {
+            reject(err);
+          }
+        });
       }
 
       if (this.state === REJECTED) {
-        try {
-          const x = onRejected(this.reason);
-          resolve(x);
-        } catch(err) {
-          reject(err);
-        }
+        process.nextTick(() => {
+          try {
+            const x = onRejected(this.reason);
+            resolve(x);
+          } catch (err) {
+            reject(err);
+          }
+        });
       }
 
       if (this.state === PENDING) {
         this.onFulfilledCallbacks.push(() => {
-          try {
-            const x = onFulfilled(this.value);
-            resolve(x);
-          } catch(err) {
-            reject(err);
-          }
+          process.nextTick(() => {
+            try {
+              const x = onFulfilled(this.value);
+              resolve(x);
+            } catch (err) {
+              reject(err);
+            }
+          });
         });
 
         this.onRejectedCallbacks.push(() => {
-          try {
-            const x = onRejected(this.reason);
-            resolve(x);
-          } catch(err) {
-            reject(err);
-          }
+          process.nextTick(() => {
+            try {
+              const x = onRejected(this.reason);
+              resolve(x);
+            } catch (err) {
+              reject(err);
+            }
+          });
         });
       }
-    })
-
-  }
+    });
+  };
 
   catch = (onRejected) => {
     if (this.state === REJECTED) {
@@ -85,46 +94,68 @@ class MyPromise {
     if (this.state === PENDING) {
       this.onRejectedCallbacks.push(() => {
         onRejected(this.reason);
-      })
+      });
     }
-  }
+  };
 }
 
-const myPromise = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Success!");
-  }, 1000);
-});
+// // test
+// const myPromise = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Success!");
+//   }, 1000);
+// });
 
-// 第一个成功回调
-myPromise.then((value) => {
-  console.log("First then:", value);
-});
+// // 第一个成功回调
+// myPromise.then((value) => {
+//   console.log("First then:", value);
+// });
 
-// 第二个成功回调
-myPromise.then((value) => {
-  console.log("Second then:", value);
-});
+// // 第二个成功回调
+// myPromise.then((value) => {
+//   console.log("Second then:", value);
+// });
 
-// 第三个成功回调
-myPromise.then((value) => {
-  console.log("Third then:", value);
-});
+// // 第三个成功回调
+// myPromise.then((value) => {
+//   console.log("Third then:", value);
+// });
 
-console.log('second test -------------------');
-console.log('');
+// console.log("second test: 链式调用 -------------------");
+// console.log("");
 
-new MyPromise((resolve, reject) => {
-  resolve(1);
-})
-.then(result => {
-  console.log(result);  // 输出 1
-  return result + 1;
-})
-.then(result => {
-  console.log(result);  // 输出 2
-  return result + 1;
-})
-.then(result => {
-  console.log(result);  // 输出 3
-});
+// new MyPromise((resolve, reject) => {
+//   resolve(1);
+// })
+//   .then((result) => {
+//     console.log(result); // 输出 1
+//     return result + 1;
+//   })
+//   .then((result) => {
+//     console.log(result); // 输出 2
+//     return result + 1;
+//   })
+//   .then((result) => {
+//     console.log(result); // 输出 3
+//   });
+
+// console.log("third test: 异步调用 -------------------");
+// console.log("");
+
+// new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1);
+//   }, 1000);
+// })
+//   .then((result) => {
+//     console.log(result); // 输出 1
+//     return result + 1;
+//   })
+//   .then((result) => {
+//     console.log(result); // 输出 2
+//     return result + 1;
+//   })
+//   .then((result) => {
+//     console.log(result); // 输出 3
+//   });
+// console.log("after output");
