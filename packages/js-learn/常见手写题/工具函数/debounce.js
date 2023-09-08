59e36a434afc8b0e9da78e2d24d96e8e17d399ce
kcfuler <kcfuler@gmail.com>
2023-09-08 16:43:24 +0800

    feat: 修正debounce写法

--- packages/js-learn/常见手写题/工具函数/debounce.js
+++ packages/js-learn/常见手写题/工具函数/debounce.js
@@ -1,11 +1,10 @@
 // 简单实现
 function debounce(fn, delay) {
   let timer = null;
-  return function () {
-    // 这里需要普通形式的函数来得到参数对象
+  return (...args) => {
     clearTimeout(timer); // 每次都先清除，这样只要没到delay的间隔，就不会触发函数
     timer = setTimeout(() => {
-      fn.apply(this, arguments); // 注意this的指向
+      fn(...args);
     }, delay);
   };
 }
