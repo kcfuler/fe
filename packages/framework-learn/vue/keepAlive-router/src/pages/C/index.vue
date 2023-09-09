9848df6f9e76aaaa709c847def468dfaa15104e1
kcfuler <kcfuler@gmail.com>
2023-09-09 16:52:27 +0800

    feat: vue: keep-alive + router

---
+++ packages/framework-learn/vue/keepAlive-router/src/pages/C/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>C页面</div>
+    <input type="text" v-model="dataC" />
+    <div @click="toA">跳转A</div>
+  </div>
+</template>
+
+<script lang="ts" setup name="C">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const dataC = ref("");
+const toA = () => {
+  router.push("/aa");
+};
+</script>
