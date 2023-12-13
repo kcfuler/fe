7a50e3f39349d469313b27ec68a71098d95aa1c4
kcfuler <kcfuler@gmail.com>
2023-12-14 00:55:45 +0800

    feat: 增加对并发线程池的理解

--- packages/js-learn/常见手写题/工具函数/并发限制.js
+++ packages/js-learn/常见手写题/工具函数/并发限制.js
@@ -1,3 +1,17 @@
+/**
+ * 使用场景：
+ * 运行并发任务，这些任务是互相独立的
+ */
+
+/**
+ * 问题：
+ * 1. 如何保证后面的任务会在前面的任务完成之后执行?
+ * 因为在前面的执行中调用了 _processQueue 方法，而这个方法的执行时机是在前面的task执行之后
+ * 两种情况:
+ *  - 如果没满，就会直接执行task
+ *  - 如果满了，那么推入队列，在正在执行中的任务执行完毕之后就执行
+ */
+
 class ConcurrencyPool {
   constructor(maxConcurrency) {
     this.maxConcurrency = maxConcurrency;
@@ -37,7 +51,7 @@ class ConcurrencyPool {
 const pool = new ConcurrencyPool(5); // 最大并发数设置为 5
 for (let i = 0; i < 10; i++) {
   pool
-    .run(() => fetch(`https://example.com/data/${i}`))
+    .run(() => fetch(`https://www.rust-lang.org/`))
     .then((response) => {
       // 处理响应
     });
