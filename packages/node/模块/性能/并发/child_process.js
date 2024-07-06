a455c21f1a5ebd80b4164894b5c987a5dec42212
kechangpeng <kechangpeng.kcp@alibaba-inc.com>
2024-07-06 20:07:23 +0800

    feat: udpate ;

--- packages/node/模块/性能/并发/child_process.js
+++ packages/node/模块/性能/并发/child_process.js
@@ -1,19 +1,16 @@
-import {spawn, fork, exec, execFile} from 'child_process';
-
-// 在 Windows 上，使用 'cmd' 命令和 '/c' 参数来执行 'dir'
-const child = spawn('cmd', ['/c', 'dir']);
+import { spawn, fork, exec, execFile } from "child_process";
 
 // 捕获标准输出并将其打印到控制台
-child.stdout.on('data', (data) => {
-    console.log(`标准输出：\n${data}`);
+child.stdout.on("data", (data) => {
+  console.log(`标准输出：\n${data}`);
 });
 
 // 捕获标准错误输出并将其打印到控制台
-child.stderr.on('data', (data) => {
-    console.error(`标准错误输出：\n${data}`);
+child.stderr.on("data", (data) => {
+  console.error(`标准错误输出：\n${data}`);
 });
 
 // 注册子进程关闭事件
-child.on('close', (code) => {
-    console.log(`子进程退出码：${code}`);
+child.on("close", (code) => {
+  console.log(`子进程退出码：${code}`);
 });
--- packages/node/模块/性能/并行/cluster/cluster.js
+++ packages/node/模块/性能/并行/cluster/cluster.js
@@ -10,6 +10,8 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
+  console.log("cluster", cluster);
+
   cluster.on("exit", (worker, code, signal) => {
     console.log(`工作进程 ${worker.process.pid} 已退出`);
   });
